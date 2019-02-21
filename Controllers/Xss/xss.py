7eb614663c91688478fa5cdebdd7b49937f32b6d
hrahmatwahyu <wahyu@gits.co.id>
2019-02-21 23:17:08 +0700

    first commit

---
+++ Controllers/Xss/xss.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+
+class LinkCrawler():
+
+    def __init__(self, url):
+        self.url = url
+
+    def RequestHtml(self):
+        try: 
+            isHtml = requests.get(self.url, timeout=10)
+        except (TimeoutError) as e:
+            print ("[+] Oops Request timeout")
+            sys.exit(0)
+        if (isHtml.status_code == 200):
+            print (isHtml.text)
+        else:
+            print ("[+] Oops response is bad")
+            sys.exit(0)
+
+
---
+++ apps.py
@@ -0,0 +1,8 @@
+from Controllers.Xss.xss import *
+
+def main():
+      tes = LinkCrawler("https://www.example.com/a")
+      tes.RequestHtml()
+
+if __name__ == "__main__":
+    main()
