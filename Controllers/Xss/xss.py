8803ac408868a2dd69486c47a67e67a5da27587f
hrahmatwahyu <wahyu@gits.co.id>
2019-02-22 10:47:03 +0700

    add crawler tags

--- Controllers/Xss/xss.py
+++ Controllers/Xss/xss.py
@@ -2,14 +2,17 @@ import requests
 import sys
 from urllib import parse
 import urllib.parse as urlparse
+from bs4 import BeautifulSoup
 
 isCheck= ['"', '>', '<', '(',')','!',';','%','@',"'"]
 
 class LinkCrawler():
 
-    def __init__(self, url):
+    def __init__(self, url, tags):
         self.url = url
+        self.tags = tags
 
+   
     def ChangeQueryParamsValues(self, isUrl, isCheckChar):
         parsed = urlparse.urlparse(isUrl)
         querys = parsed.query.split("&")
@@ -30,13 +33,15 @@ class LinkCrawler():
 
     def RequestHtml(self):
         url = self.url
+        tags = self.tags
         isUrl = self.ChangeQueryParamsValues(url, isCheck)
+        print ("[+] Checking from "+tags+" tags html")
         for i in isUrl:
             try: 
-                print (i)
                 isHtml = requests.get(i, timeout=10)
                 if (isHtml.status_code == 200):
                     print ("[+] Get HTML data from URL ..")
+                    self.Parser(isHtml.text, tags)
                 else:
                     print ("[!] Oops response is bad in url", str(i))
                     sys.exit(0)
@@ -45,4 +50,15 @@ class LinkCrawler():
                 sys.exit(0)
             
 
-    # def Parser()
+    def Parser(self, isHtmlResponse, isTagsHtml):
+        isHtml = ''
+        if (isTagsHtml == 'all') : 
+            isHtml = isHtmlResponse
+        else :
+            isParsedHtml = BeautifulSoup(isHtmlResponse,'html.parser')
+            html = isParsedHtml.find_all(isTagsHtml)
+            for x in html:
+                tags = str(x).replace(" ", "")
+                isHtml += tags
+
+        return isHtml
